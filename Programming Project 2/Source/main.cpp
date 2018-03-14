afba9bc76db3913be597279da86cf8f0a04ba50d
Tut <tutsandallthat@gmail.com>
2018-03-14 15:30:20 -0400

    main.cpp fix

--- Programming Project 2/Source/main.cpp
+++ Programming Project 2/Source/main.cpp
@@ -16,8 +16,55 @@ using std::endl;
 
 int main() {
 
-    InputValidation callValid; // instanciate object of class
+    // Instantiate objects of classes that we will use later
+    Times time;
+    Transportation trans;
+    TravelExpenses travel;
+
+    // Declare all needed variables
+    std::string employeeName;
+    int totalDays;
+    double  departTime,
+            returnTime,
+            totalAirfareCost,
+            totalRentalCost,
+            totalMileageCost,
+            totalParkingFees,
             totalTaxiFees,
+            totalRegistrationCost,
+            totalHotelCost,
+            totalMealCost;
+
+    cout << "Employee name: "; //Get employee name
+    cin >> employeeName;
+
+    cout << "How many days were spent on the trip? "; //Get days spent on trip. Called from the Times.cpp class.
+    totalDays = time.daysSpent();
+
+    time.times(departTime, returnTime); //Get departure and return times.
+
+    // AS OF 3/14/18 NO FUNCTIONS HAVE BEEN DEFINED PAST THIS POINT
+
+    cout << "Enter the amount of airfare: "; //Get total airfare costs
+    totalAirfareCost = trans.airFare();
+
+    cout << "Enter the amount spent for car rental: "; //Get total car rental costs
+    totalRentalCost = trans.carRental();
+
+    cout << "Enter the amount of miles driven by private vehicle: "; //Get cost of total miles driven
+    totalMileageCost = trans.vehicle();
+
+    cout << "Enter the amount of parking fees: "; //Get parking fees
+    trans.parking(totalDays, CompanyCosts::MAX_PARKING_FEE_PER_DAY, totalParkingFees);
+
+    cout << "Enter the amount of taxi fees: "; //Get total taxi fees
+    trans.taxi(totalDays, CompanyCosts::MAX_TAXI_FEE_PER_DAY, totalTaxiFees);
+
+    cout << "Enter the amount of conference registration: "; //Get registration costs
+    totalRegistrationCost = travel.registration();
+
+    travel.hotel(totalDays, CompanyCosts::MAX_HOTEL_COST_PER_NIGHT, totalHotelCost); //Get total meal costs
+    travel.meals(totalDays, departTime, returnTime, totalMealCost);
 
     // Debug Code : cout << departTime << " " << returnTime << " " << totalDays;
 
