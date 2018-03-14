d2d367ed14e32d38e632fb8aae33ea37d072911d
Excaliburns <tutsandallthat@gmail.com>
2018-03-14 11:11:20 -0400

    Update main.cpp

--- Programming Project 2/Source/main.cpp
+++ Programming Project 2/Source/main.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include "InputValidation.cpp"
 
+// #include "../Headers/CompanyCosts.h" ** INCLUDE THIS IF YOU ARE USING THE VARIABLES IN CompanyCosts.h"
+
 using std::cout;
 using std::endl;
 
@@ -14,11 +16,13 @@ int main() {
 
     InputValidation callValid; // instanciate object of class
 
-
     double totalTripDays;
 
     cout << "How many days are going to be spent on the trip?";
     totalTripDays = callValid.UserInput();
-
+    
+    // totalTripDays = PROGRAMMING_PROJECT_2_COMPANYCOSTS_H::MAX_TAXI_FEE_PER_DAY; ** EXAMPLE CODE. **
+    // Above is an example of how you might use the variables that are defined in CompanyCosts.h
+    
     return 0;
 }
