54c4b63e3714252ae48f3f66da22f725b77a74bb
Excaliburns <tutsandallthat@gmail.com>
2018-03-14 11:05:14 -0400

    Update and rename calc.h to InputValidation.h

---
+++ Programming Project 2/Headers/InputValidation.h
@@ -0,0 +1,13 @@
+#ifndef PROGRAMMING_PROJECT_2_INPUTVALIDATION_H
+#define PROGRAMMING_PROJECT_2_INPUTVALIDATION_H
+
+class InputValidation
+{
+
+public:
+    double UserInput(); // In header files, declare but DO NOT define functions.
+
+};
+
+
+#endif //PROGRAMMING_PROJECT_2_INPUTVALIDATION_H
