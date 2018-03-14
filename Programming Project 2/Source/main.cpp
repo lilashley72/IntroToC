bd61a6ceca1b5b041f6084b853f1ebab32c87a2c
Excaliburns <tutsandallthat@gmail.com>
2018-03-14 11:02:02 -0400

    Update main.cpp

--- Programming Project 2/Source/main.cpp
+++ Programming Project 2/Source/main.cpp
@@ -5,8 +5,20 @@
  */
 
 #include <iostream>
+#include "InputValidation.cpp"
+
+using std::cout;
+using std::endl;
 
 int main() {
+
+    InputValidation callValid; // instanciate object of class
+
+
+    double totalTripDays;
+
+    cout << "How many days are going to be spent on the trip?";
+    totalTripDays = callValid.UserInput();
 
     return 0;
 }
